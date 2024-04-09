90a8d0f31e5ea673791abbce9acdc14a558a7493
patrick <tpdud285@gmail.com>
2024-04-09 14:59:25 +0900

    상속관계매핑

--- src/main/java/hellojpa/BaseEntity.java
+++ src/main/java/hellojpa/BaseEntity.java
@@ -1,9 +1,15 @@
 package hellojpa;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public class BaseEntity {
-
+@MappedSuperclass
+@Getter @Setter
+public abstract class BaseEntity {
+//JPA에서 아래 칼럼들은 다 자동화 가능
     private String createdBy;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
