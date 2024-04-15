d83b92fcf9cef4f7b076c9712fb4f92fd8f3e467
patrick <tpdud285@gmail.com>
2024-04-15 15:13:13 +0900

    컬렉션 값 타입 사용법 -> 실무에서는 잘 안쓰고 엔티티를 쓰자

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,46 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setActor("길동");
-            movie.setDirector(("bbb"));
-            movie.setName("바람");
-            movie.setPrice(1000);
-            em.persist(movie);
+//            Movie movie = new Movie();
+//            movie.setActor("길동");
+//            movie.setDirector(("bbb"));
+//            movie.setName("바람");
+//            movie.setPrice(1000);
+//            em.persist(movie);
+
+//            Member member = new Member();
+//            member.setName("hello");
+//            member.setHomeAddress(new Address("city", "street", "zipcode"));
+            Member member = new Member();
+            member.setName("hello");
+            member.setHomeAddress(new Address("city", "street", "zipcode"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("city1", "street1", "zipcode1"));
+            member.getAddressHistory().add(new Address("city2", "street2", "zipcode2"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("========================== START ==========================");
+            Member findMember = em.find(Member.class, member.getId());
+            // city -> newCity
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            //치킨 -> 한식 (String이기 때문에 지우고 새로넣어야한다)
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            //equals, hashcode가 선언돼있어야한다. embedded타입이라서
+            findMember.getAddressHistory().remove(new Address("city1", "street1", "zipcode1"));
+            findMember.getAddressHistory().add(new Address("newCity", "street1", "zipcode1"));
+            tx.commit();
         }catch (Exception e){
             e.printStackTrace();
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Getter @Setter
 public class Member extends BaseEntity{
@@ -14,7 +19,17 @@ public class Member extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
-
+    @Embedded
+    private Address homeAddress;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
     public void changeTeam(Team team){
         this.team = team;
         team.getMembers().add(this);
